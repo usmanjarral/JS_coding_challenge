7a994d6438c2686df6f216cfdc59cee1a8628f70
usmanjarral <usmanjarral2003@gmail.com>
2024-07-04 23:54:13 +0500

    done

---
+++ script.js
@@ -0,0 +1,13 @@
+// Write a program which does the following:
+
+// Find the area of a circle whose radius is 8.9. Take pi = 3.14
+// Declare variables radius , pi and area and assign the relevant values to them
+// Output the area, you dont need to output any other text.
+
+
+const pi = 3.14;
+
+let radius = 8.9;
+let areaOfCircle = pi*radius*radius;
+
+console.log(areaOfCircle);  
